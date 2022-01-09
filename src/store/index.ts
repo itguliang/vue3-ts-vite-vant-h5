235d0a5fc4eb84ec618f42b488347eeb68156d3b
itguliang <1072625341@qq.com>
2022-01-09 22:29:44 +0800

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,11 @@
 import { createStore } from 'vuex'
+
+import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
+
+const state={}
+
 let modules = {}
 // Vite 支持 Glob 导入 https://vitejs.cn/guide/features.html#glob-%E5%AF%BC%E5%85%A5
 // 导入 modules 下面的 所有 .ts文件
@@ -7,9 +14,11 @@ for (const path in modulesFiles) {
 	const moduleName = path.replace(/(.*\/)*([^.]+).*/gi, '$2')
 	modules = { ...modules, [moduleName]: modulesFiles[path].default }
 }
+
 export default createStore({
-	state: {},
-	mutations: {},
-	actions: {},
+	state,
+	getters,
+	mutations,
+	actions,
 	modules
 })
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,35 +1,35 @@
 import { getUserInfo } from '@/api/user'
 import { Store } from 'vuex';
 export interface UserState {
-  name: string,
+	name: string,
 }
 
 const state = () => ({
 	name: 'yuyu',
-  detail:Object,
+	detail: Object,
 })
 
 const getters = {}
 
 const mutations = {
-	SET_NAME (state: any, name: string) {
+	SET_NAME(state: any, name: string) {
 		state.name = name
 	},
-  SET_DETAIL (state: any, detail: string) {
+	SET_DETAIL(state: any, detail: string) {
 		state.detail = detail
 	},
 }
 
 const actions = {
-	async GetUserInfo (store: Store<UserState>) {
+	async GetUserInfo(store: Store<UserState>) {
 		const userInfo: any = await getUserInfo()
-		if(userInfo) {
+		if (userInfo) {
 			store.commit('SET_DETAIL', userInfo)
 		}
 	},
-  updateName(store: Store<UserState>){
-    store.commit('SET_NAME', '90909090')
-  }
+	updateName(store: Store<UserState>) {
+		store.commit('SET_NAME', '90909090')
+	}
 	// LogOut({ commit }) {
 	// 	return new Promise(resolve => {
 	// 		console.log('登出...')
