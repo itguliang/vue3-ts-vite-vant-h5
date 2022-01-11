7297eb9ab0af7b9081c143ef2054fe826e82ec14
itguliang <1072625341@qq.com>
2022-01-11 18:24:18 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-view />
+  <div class="tab-view">
+    <router-view />
+  </div>
   <Tabbar v-model="active">
     <TabbarItem icon="home-o" to="/home">首页</TabbarItem>
     <TabbarItem icon="flower-o">学习</TabbarItem>
@@ -11,3 +13,12 @@ import { Tabbar, TabbarItem } from 'vant';
 import { ref } from 'vue';
 const active = ref(0);
 </script>
+<style>
+.tab-view {
+  /* background: #f5f5f5; */
+  position: fixed;
+  top: 0;
+  bottom: 50px;
+  width: 100%;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,50 @@
 <template>
   <Swipe :loop="true" class="my-swipe">
-    <SwipeItem>1</SwipeItem>
-    <SwipeItem>2</SwipeItem>
-    <SwipeItem>3</SwipeItem>
-    <SwipeItem>4</SwipeItem>
+    <SwipeItem>
+      <div class="banner">
+        <VanImage :src="banner1" />
+      </div>
+    </SwipeItem>
+    <SwipeItem>
+      <div class="banner">
+        <VanImage :src="banner2" />
+      </div>
+    </SwipeItem>
+    <SwipeItem>
+      <div class="banner">
+        <VanImage :src="banner3" />
+      </div>
+    </SwipeItem>
   </Swipe>
+
+  <Grid :border="false">
+    <GridItem icon="fire-o" text="Vue" />
+    <GridItem icon="fire-o" text="React" />
+    <GridItem icon="fire-o" text="Angular" />
+    <GridItem icon="fire-o" text="Nodejs" />
+    <GridItem icon="fire-o" text="ES6" />
+    <GridItem icon="fire-o" text="Typescript" />
+    <GridItem icon="fire-o" text="Webpack" />
+    <GridItem icon="fire-o" text="更多" />
+  </Grid>
+
+  <CellGroup inset>
+    <Cell title="单元格" value="内容" />
+    <Cell title="单元格" value="内容" label="描述信息" />
+  </CellGroup>
   <div>这是home信息-{{ store.state.home.info }}</div>
   <div>这是user信息-{{ store.state.user.name }}</div>
   <Button type="primary" @click="updateName">axios 调用接口信息</Button>
 </template>
 
 <script setup lang="ts">
-import { Swipe, SwipeItem, Button } from 'vant';
+import { Swipe, SwipeItem, Button, Cell, CellGroup, Image as VanImage, Grid, GridItem } from 'vant';
 import { useStore } from "vuex";
 import { onMounted } from "vue";
+
+import banner1  from "@/assets/banner1.png";
+import banner2  from "@/assets/banner2.png";
+import banner3  from "@/assets/banner3.png";
 
 const store = useStore();
 
@@ -28,11 +59,12 @@ onMounted(() => {
 </script>
 
 <style>
-  .my-swipe .van-swipe-item {
-    color: #fff;
-    font-size: 20px;
-    line-height: 150px;
-    text-align: center;
-    background-color: #39a9ed;
-  }
+.banner{
+  margin: 16px 16px 0 16px;
+  border-radius: 10px;
+  overflow: hidden;
+}
+.my-swipe {
+  margin: 0 0 16px 0;
+}
 </style>
